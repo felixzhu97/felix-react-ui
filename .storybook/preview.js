fb09f317bbca7d8d9e81d3062f84937231773481
felix.zhu <1434866867@qq.com>
2025-06-15 12:37:21 +0800

    chore: Remove Storybook from .gitignore and add .storybook directory
    - Storybook configuration is now tracked in version control
    - Removed .storybook ignore rule as the directory is needed for the project

---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    docs: {
+      description: {
+        component: "Felix UI 组件库",
+      },
+    },
+  },
+};
+
+export default preview;
